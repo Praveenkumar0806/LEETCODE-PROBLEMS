67098dc4a75aa858a61b00b7c050b42efca821cc
Praveenkumar <swagyboypraveen0806@gmail.com>
2026-09-16 13:52:54 +0530

    Add files via upload

---
+++ AddTwoIntegers.java
@@ -0,0 +1,11 @@
+
+class AddTwoIntegers{
+    public static int sum(int num1, int num2) {
+        return num1+num2;
+    }
+    public static void main(String[] args){
+	int num1=10;
+	int num2=1;
+	int result = sum(num1,num2);
+	System.out.println(result);	
+}
